23aa6e1f805467108249e3c5924ae6a9dc92ab3e
Credwa <craigroe7@gmail.com>
2018-03-07 23:25:04 -0500

    remove test route

--- server.js
+++ server.js
@@ -8,10 +8,6 @@ let app = express();
 
 app.use(device.capture());
 
-app.get('/test', function(req, res) => {
-
-})
-
 app.get('/', function(req, res) {
   var ua = req.headers['user-agent'],
     $ = {};
