17bf9d8a1e983a786328231d378e0f89465bd2b4
Credwa <craigroe7@gmail.com>
2018-03-07 23:24:29 -0500

    update

--- server.js
+++ server.js
@@ -2,20 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const device = require('express-device');
+const axios = require('axios');
 const port = process.env.PORT || 3000;
 let app = express();
 
 app.use(device.capture());
 
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
+app.get('/test', function(req, res) => {
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE')
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+})
 
 app.get('/', function(req, res) {
   var ua = req.headers['user-agent'],
@@ -35,10 +30,6 @@ app.get('/', function(req, res) {
     app.use(express.static(__dirname + '/dist/pwa-mat'));
     res.sendFile(path.join(__dirname + '/dist/pwa-mat/index.html'));
   }
-});
-
-app.get('/test', function(req, res) {
-  console.log(req.device.type.toUpperCase());
 });
 
 app.listen(port, () => {
